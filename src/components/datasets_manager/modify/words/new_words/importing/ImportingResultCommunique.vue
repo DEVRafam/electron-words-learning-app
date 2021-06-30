4957b86ce7adfd341d4d1fcb0544fff026277207
DEVRafam <jodax95@gmail.com>
2021-06-30 20:55:52 +0200

    ⚙ Datasets Manager- Importing Result Comunique messages removed

--- src/components/datasets_manager/modify/words/new_words/importing/ImportingResultCommunique.vue
+++ src/components/datasets_manager/modify/words/new_words/importing/ImportingResultCommunique.vue
@@ -3,15 +3,10 @@
         <!--  -->
         <template v-if="importingResult == 'negative'">
             <font-awesome-icon icon="times-circle"></font-awesome-icon>
-            <h1>An error has occured</h1>
         </template>
         <!--  -->
         <template v-else-if="importingResult == 'positive'">
             <font-awesome-icon icon="check-circle"></font-awesome-icon>
-            <h1>
-                <strong>{{ amountOfImportedWords }} </strong>
-                <span> words {{ hasOrHave }} been imported successfully </span>
-            </h1>
         </template>
     </span>
 </template>
